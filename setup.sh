7ed737768f638fbe9f8c934f86490e1524576a04
Julian <julian.teh@screeningeagle.com>
2023-02-28 14:56:30 +0800

    Updating environment for MacOS to add more paths

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+# Install Rust and tools for Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install sccache
+
+# Generic use
+cargo install nu bat exa du-dust speedtest-rs coreutils
+
+# Generic dev
+cargo install gitui rtx-cli mprocs
+
+# Neovim
+cargo install bob-nvim
+bob install v0.8.3
+cargo install neovide ripgrep
+
+# Rust dev tools
+cargo install bacon cargo-info
+
+sudo ln -s ~/.cargo/bin/nu /usr/local/bin/nu
